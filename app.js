5bc1cf814df5483ba6d4d1a035308e33a7fbd126
Vahini Ranjith Singh <vahini.lollipop@gmail.com>
2020-07-06 23:25:32 +0530

    views,ejs,middlewares and static files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+const morgan = require('morgan');
 
 //express app
 const app = express();
@@ -8,6 +9,24 @@ app.set('view engine', 'ejs');
 
 //listen for request
 app.listen(3000);
+
+//middleware
+// app.use((req, res, next) => {
+//     console.log('new request made:');
+//     console.log('host:', req.hostname);
+//     console.log('path:', req.path);
+//     console.log('method:', req.method);
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log('In the next middleware');
+//     next();
+// });
+
+//middleware and static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
 app.get('/', (req,res) => {
     //res.send('<p>Home Page</p>');
